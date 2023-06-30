164115cdf403256c51ee8133db75d6acc9e1f78c
Crowdbotics <team@crowdbotics.com>
2023-06-30 17:40:16 +0000

    Initial commit.

---
+++ backend/city_hope_church_41828/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for city_hope_church_41828 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'city_hope_church_41828.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "city-hope-church-41828",
+  url: "https://city-hope-church-41828.botics.co",
+  api: "https://city-hope-church-41828.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
